e2c9cd57f3c7e255916c360edbb6ce2451b83bad
Mokaisen <andres.molanob@gmail.com>
2025-05-12 12:49:59 -0500

    Initial commit

---
+++ source/brownbot_rl/brownbot_rl/tasks/manager_based/brownbot_rl/brownbot.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for the Universal Robots of brownbotics.
+
+Reference: https://github.com/ros-industrial/universal_robot
+"""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+
+BROWNBOT05_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"/isaac-sim/workspaces/isaac_sim_scene/brownbot_05.usd",
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        activate_contact_sensors=False,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "shoulder_pan_joint": 0.0,
+            "shoulder_lift_joint": -1.712,
+            "elbow_joint": 1.712,
+            "wrist_1_joint": -1.712,
+            "wrist_2_joint": -1.712,
+            "wrist_3_joint": 1.712,
+            "finger_joint": 0.0,
+        },
+    ),
+    actuators={
+        "arm_00": ImplicitActuatorCfg(
+            joint_names_expr=["shoulder_pan_joint"],
+            velocity_limit=1.220,
+            effort_limit=150.0,
+            stiffness=261.7,
+            damping=26.17,
+        ),
+        "arm_01": ImplicitActuatorCfg(
+            joint_names_expr=["shoulder_lift_joint"],
+            velocity_limit=1.220,
+            effort_limit=150.0,
+            stiffness=261.0,
+            damping=26.1,
+        ),
+        "arm_02": ImplicitActuatorCfg(
+            joint_names_expr=["elbow_joint"],
+            velocity_limit=1.220,
+            effort_limit=150.0,
+            stiffness=261.0,
+            damping=26.0,
+        ),
+        "arm_03": ImplicitActuatorCfg(
+            joint_names_expr=["wrist_1_joint"],
+            velocity_limit=1.220,
+            effort_limit=28.0,
+            stiffness=261.0,
+            damping=26.0,
+        ),
+        "arm_04": ImplicitActuatorCfg(
+            joint_names_expr=["wrist_2_joint"],
+            velocity_limit=1.220,
+            effort_limit=28.0,
+            stiffness=261.0,
+            damping=26.0,
+        ),
+        "arm_05": ImplicitActuatorCfg(
+            joint_names_expr=["wrist_3_joint"],
+            velocity_limit=1.220,
+            effort_limit=28.0,
+            stiffness=261.0,
+            damping=26.0,
+        ),
+        "arm_06": ImplicitActuatorCfg(
+            joint_names_expr=["finger_joint"],
+            velocity_limit=1.220,
+            effort_limit=10.0,
+            stiffness=0.1125, #300
+            damping=0.001, #30
+        ),
+    },
+)
+"""Configuration of UR-10 arm using implicit actuator models."""
